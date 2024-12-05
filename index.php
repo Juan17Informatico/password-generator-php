4a50fa6d7606f4c3344c3ecca4773dde85f5a338
Juan17Informatico <juancampuzano2356@gmail.com>
2024-12-04 23:11:35 -0500

    Se agrega el array de los caracteres permitidos

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Definir arrays de caracteres
+$uppercase = range('A', 'Z'); // Letras mayúsculas
+$lowercase = range('a', 'z'); // Letras minúsculas
+$numbers = range(0, 9);       // Números
+$specialCharacters = ['@', '#', '$', '%', '&', '*', '!', '?', '-', '_', '+', '='];
+
