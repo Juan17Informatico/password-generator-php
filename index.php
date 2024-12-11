7cb2772fe21f2fc2189872ebc5f59ea8c689fd93
JuanCM <juancampuzano2356@gmail.com>
2024-12-11 16:11:43 -0500

    feat(Logic) Se crea la logica para generar contraseña: Se crea logica de la aplicación para generar de manera dinamica contraseñas según dos parametros - Numero de caracteres - int - Caracteres permitidos - Array

--- index.php
+++ index.php
@@ -6,3 +6,28 @@ $lowercase = range('a', 'z'); // Letras minúsculas
 $numbers = range(0, 9);       // Números
 $specialCharacters = ['@', '#', '$', '%', '&', '*', '!', '?', '-', '_', '+', '='];
 
+function passwordGenerator(int $numberChars, array $allowedChars) : string
+{
+
+    if ($numberChars > 30 and $numberChars < 1 ) {
+        throw new InvalidArgumentException("Cantidad de caracteres no permitida");
+    }
+
+    if(empty($allowedChars)){
+        throw new InvalidArgumentException("El array de caracteres permitidos no puede estar vacío");
+    }
+
+    $generatedPassword = ''; 
+
+    for($i = 0;$i < $numberChars; $i++) {
+        $randomIndex = array_rand($allowedChars);
+        $generatedPassword .= $allowedChars[$randomIndex];
+    }
+
+    return $generatedPassword;
+    
+}
+
+$allowedCharsArray = array_merge($uppercase, $numbers, $lowercase, ['@', '#', '$']);
+
+print_r(passwordGenerator(29, $allowedCharsArray));
