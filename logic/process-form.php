23454e6c8eb23dbd4a4b2d678f69d163324c0da5
JuanCM <juancampuzano2356@gmail.com>
2024-12-16 14:01:03 -0500

    Se agregan estilos al resultado del formulario - Se agregan opciones de volver a inicio y de generar una contraseña nueva con los mismos parametros

--- logic/process-form.php
+++ logic/process-form.php
@@ -4,8 +4,12 @@ require_once('./password_generator.php');
 
 $request = $_REQUEST;
 
-$chars = charsAllowed($request["chars"]);
-$password = passwordGenerator($request["charsNumbers"], $chars);
+$charsInputValue = $request["chars"];
+
+$charsAmountInputValues = $request["charsNumbers"];
+
+$chars = charsAllowed($charsInputValue);
+$password = passwordGenerator( $charsAmountInputValues, $chars);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +18,7 @@ $password = passwordGenerator($request["charsNumbers"], $chars);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+    <link rel="stylesheet" href="../styles/process_form.css">
 </head>
 
 <body>
@@ -23,7 +28,19 @@ $password = passwordGenerator($request["charsNumbers"], $chars);
                 <h2>Resultado</h2>
             </div>
             <div class="contain">
-                <p><?= $password ?></p>
+                <div class="result-container">
+                    <p style="text-align: center;" ><?= $password ?></p>
+                </div>
+                <div class="back-container">
+                    <form  class="form-btn" action="process-form.php" method="POST" >
+                        <?php foreach ($charsInputValue as $char) : ?>
+                            <input type="hidden" name="chars[]" value="<?= htmlspecialchars($char) ?>" >
+                        <?php endforeach; ?>
+                        <input type="hidden" name="charsNumbers" value="<?= $charsAmountInputValues ?>"  >
+                        <button type="submit" href="process-form.php" class="back-btn">Generar Nueva Contraseña</button>
+                    </form>
+                    <a href="../index.php" class="back-btn">Volver a inicio</a>
+                </div>
             </div>
         </div>
     </main>
